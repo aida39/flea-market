5ea2f331dbe32a0cca83bc3fd7a923187fce6daa
aida <alma2013mtk@gmail.com>
2024-03-27 16:33:55 +0900

    トップページ作成

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,9 +1,27 @@
 @extends('layouts.header')
 
 @section('css')
-<!-- <link rel="stylesheet" href="{{ asset('css/index.css') }}" /> -->
 @endsection
 
 @section('content')
-
+<div class="tab">
+    <ul class="tab-menu">
+        <li class="tab-menu__item active">おすすめ</li>
+        <li class="tab-menu__item">マイリスト</li>
+    </ul>
+    <div class="tab-content">
+        <div class="tab-content__item show">
+            <img src="{{asset('images/item_01.jpg')}}" alt="">
+            <img src="{{asset('images/item_01.jpg')}}" alt="">
+            <img src="{{asset('images/item_01.jpg')}}" alt="">
+            <img src="{{asset('images/item_01.jpg')}}" alt="">
+            <img src="{{asset('images/item_01.jpg')}}" alt="">
+            <img src="{{asset('images/item_01.jpg')}}" alt="">
+        </div>
+        <div class="tab-content__item">
+            マイリストはありません
+        </div>
+    </div>
+</div>
+<script src="{{ asset('js/tab-menu.js') }}"></script>
 @endsection
