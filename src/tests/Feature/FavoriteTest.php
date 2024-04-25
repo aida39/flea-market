003b54ac09f60062ed9789dd1ee8625f8a81f0a2
aida <alma2013mtk@gmail.com>
2024-04-25 14:16:45 +0900

    FavoriteTest作成

---
+++ src/tests/Feature/FavoriteTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\Item;
+use App\Models\User;
+
+class FavoriteTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $user;
+    protected $item;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+        $this->actingAs($this->user);
+
+        $this->seed(\Database\Seeders\ConditionsTableSeeder::class);
+        $this->item = Item::factory()->create();
+    }
+
+    public function test_store_favorite()
+    {
+        $response = $this->patch("/favorite/{$this->item->id}");
+
+        $this->assertTrue($this->item->fresh()->favorite()->where('user_id', $this->user->id)->exists());
+    }
+
+    public function test_delete_favorite()
+    {
+        $this->item->favorite()->create(['user_id' => $this->user->id]);
+
+        $response = $this->patch("/favorite/{$this->item->id}");
+
+        $this->assertFalse($this->item->fresh()->favorite()->where('user_id', $this->user->id)->exists());
+    }
+}
