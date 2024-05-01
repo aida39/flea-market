69eac6a17703cfd051e16d2bf6bf811b95928e28
aida <alma2013mtk@gmail.com>
2024-05-01 16:25:17 +0900

    Admin/LoginTest作成、Admin/MailTest作成

--- src/tests/Feature/Admin/LoginTest.php
+++ src/tests/Feature/Admin/LoginTest.php
@@ -3,20 +3,49 @@
 namespace Tests\Feature\Admin;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Admin;
 
 class LoginTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    use RefreshDatabase;
+
+    protected $admin;
+
+    protected function setUp(): void
     {
-        $response = $this->get('/');
+        parent::setUp();
+        $this->seed(\Database\Seeders\AdminsTableSeeder::class);
+        $this->admin = Admin::first();
+    }
+
+    public function test_login_page()
+    {
+        $response = $this->get('/login');
 
         $response->assertStatus(200);
+    }
+
+    public function test_admin_login()
+    {
+        $login_data = [
+            'email' => 'admin@example.com',
+            'password' => 'coachtech',
+        ];
+        $response = $this->post('/admin/login', $login_data);
+
+        $response->assertRedirect('/admin/index');
+        $this->assertAuthenticatedAs($this->admin, 'admin');
+        $response->assertStatus(302);
+    }
+
+    public function test_admin_logout()
+    {
+        $this->actingAs($this->admin, 'admin');
+        $response = $this->get('/admin/logout');
+
+        $response->assertRedirect('/admin/login');
+        $this->assertGuest();
+        $response->assertStatus(302);
     }
 }
--- src/tests/Feature/Admin/MailTest.php
+++ src/tests/Feature/Admin/MailTest.php
@@ -3,20 +3,54 @@
 namespace Tests\Feature\Admin;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Mail;
 use Tests\TestCase;
+use App\Models\Admin;
+use App\Models\User;
+use App\Mail\AdminMail;
 
 class MailTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    use RefreshDatabase;
+
+    protected $admin;
+
+    protected function setUp(): void
     {
-        $response = $this->get('/');
+        parent::setUp();
+        $this->seed(\Database\Seeders\AdminsTableSeeder::class);
+        $this->admin = Admin::first();
+    }
+
+    public function test_mail_form()
+    {
+        $this->actingAs($this->admin, 'admin');
+
+        $response = $this->get('/admin/mail');
 
         $response->assertStatus(200);
+        $this->assertAuthenticatedAs($this->admin, 'admin');
+    }
+
+    public function test_send_mail()
+    {
+        $this->actingAs($this->admin, 'admin');
+
+        User::factory()->create();
+
+        Mail::fake();
+        $data = [
+            'mail_subject' => 'テストメール',
+            'mail_message' => 'これはテストメールです。',
+        ];
+
+        $response = $this->post('/admin/mail', $data);
+
+        $response->assertRedirect('/admin/mail');
+        $response->assertStatus(302);
+
+        $this->assertAuthenticatedAs($this->admin, 'admin');
+
+        Mail::assertSent(AdminMail::class);
     }
 }
