58aa32b655468f19d2956477608f84b1bf834bad
aida <alma2013mtk@gmail.com>
2024-05-02 17:10:36 +0900

    不要なファクトリ・マイグレーションファイルの削除、ignoreMigrations()追加

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,26 +3,19 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Cashier\Cashier;
+use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
+    public function boot()
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
+    public function register()
     {
-        //
+        Cashier::ignoreMigrations();
+        Sanctum::ignoreMigrations();
     }
 }
